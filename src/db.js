e524ad1ae7e9286fa0a7afa3cff7e6eaf6d76f16
Suraj Verma <surajverma@rentalcars.com>
2021-07-09 15:42:47 +0100

    log successful mongoose connection

--- src/db.js
+++ src/db.js
@@ -5,6 +5,7 @@ const { DB_URL, MONGOOSE_OPTIONS } = require("./config");
 const connect = async () => {
   try {
     await mongoose.connect(DB_URL, MONGOOSE_OPTIONS);
+    console.log("Successfully connected to mongoDB");
   } catch (error) {
     throw error;
   }
