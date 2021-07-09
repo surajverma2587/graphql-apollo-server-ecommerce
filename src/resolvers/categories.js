7a16b9a08425e808de9abf4d778bc078059d9ba9
Suraj Verma <surajverma@rentalcars.com>
2021-07-09 16:22:40 +0100

    update tags and categories resolver to read from db

--- src/resolvers/categories.js
+++ src/resolvers/categories.js
@@ -1,27 +1,8 @@
-const categoriesData = [
-  {
-    id: "a11e1d9e-e09d-11eb-ba80-0242ac130004",
-    name: "Shirts",
-  },
-  {
-    id: "a11e1ff6-e09d-11eb-ba80-0242ac130004",
-    name: "Shorts",
-  },
-  {
-    id: "a11e20dc-e09d-11eb-ba80-0242ac130004",
-    name: "Music",
-  },
-  {
-    id: "a11e21a4-e09d-11eb-ba80-0242ac130004",
-    name: "Hats",
-  },
-  {
-    id: "a11e2258-e09d-11eb-ba80-0242ac130004",
-    name: "Shoes",
-  },
-];
+const { Category } = require("../models");
 
-const categories = () => {
+const categories = async () => {
+  const categoriesData = await Category.find({});
+
   return categoriesData;
 };
 
--- src/resolvers/tags.js
+++ src/resolvers/tags.js
@@ -1,39 +1,7 @@
-const tagsData = [
-  {
-    id: "d47a7a5c-e09d-11eb-ba80-0242ac130004",
-    name: "rock music",
-  },
-  {
-    id: "d47a7cbe-e09d-11eb-ba80-0242ac130004",
-    name: "pop music",
-  },
-  {
-    id: "d47a7dae-e09d-11eb-ba80-0242ac130004",
-    name: "blue",
-  },
-  {
-    id: "d47a8024-e09d-11eb-ba80-0242ac130004",
-    name: "red",
-  },
-  {
-    id: "d47a80f6-e09d-11eb-ba80-0242ac130004",
-    name: "green",
-  },
-  {
-    id: "d47a81b4-e09d-11eb-ba80-0242ac130004",
-    name: "white",
-  },
-  {
-    id: "d47a8268-e09d-11eb-ba80-0242ac130004",
-    name: "gold",
-  },
-  {
-    id: "d47a8326-e09d-11eb-ba80-0242ac130004",
-    name: "pop culture",
-  },
-];
+const { Tag } = require("../models");
 
-const tags = () => {
+const tags = async () => {
+  const tagsData = await Tag.find({});
   return tagsData;
 };
 
