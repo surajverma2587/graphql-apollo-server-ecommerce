d0bf28702f1d50fcdd9048479b96d25165bd852e
Suraj Verma <surajverma@rentalcars.com>
2021-07-09 11:01:25 +0100

    create typeDefs

---
+++ src/schema/index.js
@@ -0,0 +1,8 @@
+const Query = require("./Query");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const Product = require("./Product");
+
+const typeDefs = [Query, Category, Tag, Product];
+
+module.exports = typeDefs;
